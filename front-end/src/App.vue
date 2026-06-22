112b812d85578e64ef44658cec42890ab80d6856
Ornella <Ornellaormb@gmail.com>
2026-06-23 00:44:38 +0200

    page connexion

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import Navbar from './components/NavbarView.vue'
+import { useAuthStore } from '@/stores/authStores.js' 
+
+const authStore = useAuthStore()
 </script>
+
 <template>
   <div>
     <Navbar />
 
+    <button v-if="authStore.isAuthenticated" @click="authStore.logout()">Déconnexion</button>
+    <router-link v-if="authStore.userRole === 'Administrateur'" to="/admin">Espace Admin</router-link>
+    
     <main>
       <RouterView />
     </main>
